eb4a51c80066c3a5de5fcd30a2333bbbbaab325a
tianluolin_ustb@163.com <Administrator@Robintian>
2017-10-15 19:06:28 +0800

    Spring jpa

--- src/main/java/com/lin/luo/tian/spring/jpa/SpringJpaApplication.java
+++ src/main/java/com/lin/luo/tian/spring/jpa/SpringJpaApplication.java
@@ -2,6 +2,7 @@ package com.lin.luo.tian.spring.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class SpringJpaApplication {
--- src/main/java/com/lin/luo/tian/spring/jpa/controller/StudentController.java
+++ src/main/java/com/lin/luo/tian/spring/jpa/controller/StudentController.java
@@ -3,7 +3,16 @@ package com.lin.luo.tian.spring.jpa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.lin.luo.tian.spring.jpa.domain.Student;
 import com.lin.luo.tian.spring.jpa.repositry.StudentPredicteRepositry;
@@ -11,25 +20,32 @@ import com.lin.luo.tian.spring.jpa.repositry.StudentRepositry;
 import com.querydsl.core.types.Predicate;
 
 @Controller
+@RequestMapping("/dcl")
 public class StudentController {
 	@Autowired
 	private StudentRepositry studentRepositry;
-	@Autowired
-	private StudentPredicteRepositry predicteRepositry;
-		
+//	@Autowired
+//	private StudentPredicteRepositry predicteRepositry;
+
 	public List<Student> getAllStudents() {
 		return studentRepositry.findAll();
 	}
-	
+
 	public Student getStudentById(Long id) {
 		return studentRepositry.findOne(id);
 	}
-	
+
 	public Student getStudentWithName(String name) {
 		return studentRepositry.getStudentWithName(name);
 	}
-	
-	public Student getStudentWithNameAndTelephone(Predicate predicate) {
-		return predicteRepositry.findOne(predicate);
+
+//	public Student getStudentWithNameAndTelephone(Predicate predicate) {
+//		return predicteRepositry.findOne(predicate);
+//	}
+
+	@RequestMapping(value = "/students", method = RequestMethod.GET)
+	public HttpEntity<PagedResources<Student>> students(Pageable pageable, PagedResourcesAssembler assembler) {
+		Page<Student> students = studentRepositry.findAll(pageable);
+		return new ResponseEntity<>(assembler.toResource(students), HttpStatus.OK);
 	}
 }
